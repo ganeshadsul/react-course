7db1827a90fa57059aed72e8512a68838f18eddd
Ganesh Adsul <ganeshadsul76@gmail.com>
2025-12-15 20:28:16 +0530

    Using jsx instead of js

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = <h1>This is h1 tag inside jsx</h1>;
+console.log(heading);
+
+root.render(heading);
