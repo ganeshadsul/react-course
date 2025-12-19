c20edb667e5732dab882eaed56b73029903cca67
Ganesh Adsul <ganeshadsul76@gmail.com>
2025-12-20 00:35:48 +0530

    using mock data for showing restaurants and added hooks for searching and filtering Restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,53 @@
-import RestuarantCard from './RestuarantCard';
-import restuarants from '../utils/mockData';
+import { useState } from 'react';
+import RestaurantCard from './RestaurantCard';
+import mockRestaurants from '../utils/mockData';
 
 const Body = () => {
+	const [restaurants, setRestaurants] = useState(mockRestaurants);
 	return (
 		<>
-			<div className="search">Search bar</div>
+			<div className="search">
+				<input
+					type="text"
+					className="search-restaurants"
+					placeholder="Search restuarants"
+					onChange={(e) => {
+						if (e.target.value === '')
+							return setRestaurants(mockRestaurants);
+						const filteredRestaurants = mockRestaurants.filter(
+							(restaurant) =>
+								restaurant.name
+									.toLowerCase()
+									.includes(e.target.value.toLowerCase())
+						);
+						setRestaurants(filteredRestaurants);
+					}}
+				/>
+			</div>
+			<div className="filters">
+				<button
+					className="top-rated-res-btn"
+					onClick={() => {
+						const filteredRestaurants = restaurants.filter(
+							(restuarant) => restuarant.ratings >= 4.5
+						);
+						setRestaurants(filteredRestaurants);
+					}}
+				>
+					Above 4.5
+				</button>
+				<span>{restaurants.length} restaurants found.</span>
+			</div>
 			<div className="restuarant-container">
-				{restuarants.map((restuarant, index) => (
-					<RestuarantCard
+				{restaurants.map((restaurant, index) => (
+					<RestaurantCard
 						key={index}
-						name={restuarant.name}
-						cuisines={restuarant.cuisines}
-						averageCost={restuarant.averageCost}
-						ratings={restuarant.ratings}
-						discount={restuarant.discount}
-						imageSrc={restuarant.imageSrc}
+						name={restaurant.name}
+						cuisines={restaurant.cuisines}
+						averageCost={restaurant.averageCost}
+						ratings={restaurant.ratings}
+						discount={restaurant.discount}
+						imageSrc={restaurant.imageSrc}
 					/>
 				))}
 			</div>
